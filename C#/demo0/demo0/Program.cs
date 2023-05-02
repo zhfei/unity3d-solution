bfbb72f8f8471cb62c574eb6a0aaaf981171f32b
zhfei <1174977961@qq.com>
2023-05-02 23:24:37 +0800

    init：初始化

---
+++ C#/demo0/demo0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo0
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
