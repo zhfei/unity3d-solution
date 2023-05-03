8ff557c621c89ad70d329353c564be0a969f9ae0
zhfei <1174977961@qq.com>
2023-05-03 22:52:06 +0800

    feat: C#Test

--- C#/demo0/demo0/Program.cs
+++ C#/demo0/demo0/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace demo0
 {
@@ -7,6 +11,7 @@ namespace demo0
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
