8adf13f098831972fb9c2c92abb239a8341b15af
zhfei <1174977961@qq.com>
2023-05-05 23:37:38 +0800

    other: 变量读写

--- C#/demo0/demo0/Program.cs
+++ C#/demo0/demo0/Program.cs
@@ -10,8 +10,20 @@ namespace demo0
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+             
+            int num; // 变量声明是在内存中开辟一块内存空间，名称叫做num
+            num = 20; // 赋值是往这个内存空间中赋值
+            Console.WriteLine(num);
+
+            Console.WriteLine("请输入枪的名称：");
+            string gunName = Console.ReadLine();
+            Console.WriteLine("请输入弹夹容量：");
+            string zidanRongLiang = Console.ReadLine();
+            Console.WriteLine("请输入当前弹夹内子弹数量：");
+            string zidanShuLiang = Console.ReadLine();
+            Console.WriteLine("请输入剩余子弹数量：");
+            string zidanShuLiangShengYu = Console.ReadLine();
+            Console.WriteLine("请输入枪的名称：" + gunName + "请输入弹夹容量：" + zidanRongLiang);
         }
     }
 }
