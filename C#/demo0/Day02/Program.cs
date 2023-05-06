eed482a7d7c512ed24ca201d771fbffc8a808818
zhfei <1174977961@qq.com>
2023-05-06 08:37:21 +0800

    add：C#的Format格式化字符串

---
+++ C#/demo0/Day02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string gunName = "AK 47";
+            string gunCap = "300";
+
+            // 使用占位符来定义格式字符串
+            string des = string.Format("枪的名称:{0}, 枪的子弹容量:{1}", gunName, gunCap);
+            Console.WriteLine(des);
+        }
+    }
+}
