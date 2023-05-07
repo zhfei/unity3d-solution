ec17c7cc49b71f9fc328b206f11c937ab5a54663
zhfei <1174977961@qq.com>
2023-05-07 21:33:11 +0800

    add: 添加类型转换

--- C#/demo0/Day02/Program.cs
+++ C#/demo0/Day02/Program.cs
@@ -34,6 +34,43 @@ namespace Day02
             //使用场景
             bool r3 = n1 % 2 == 0;//奇数偶数判断
             int r4 = n1 % 10;//获取十进制的个位数
+
+            //3.快捷运算符
+            //快捷运算符 +=, -=, *=, /=
+
+            //4.一元运算符（符号的左边或右边只有一个变量: ++, --）， 二元， 三元(三目运算符 a>0?a:1)
+
+            //5.数据类型转换
+            //a.Parse转换：string类型转换成其他数据类型
+            string numStr = "18";
+            int num = int.Parse(numStr);
+            float numF = float.Parse(numStr);
+            //b.ToString转换：任意类型转换为string类型
+            int num2 = 22;
+            string num2Str = num2.ToString();
+            Console.WriteLine(num2Str);
+
+            //练习题：逐个取出字符串里的数字，进行相加取合
+            string lineNumStr = Console.ReadLine();
+            int lineNum = int.Parse(lineNumStr);
+            int num0 = lineNum/10 % 10; //十位
+            int num1 = lineNum / 100 % 10; //百位
+
+            char numC = lineNumStr[0];
+            string num22Str = numC.ToString();
+            int num22 = int.Parse(num2Str);
+
+
+            #region
+            // 在VS加一个折叠符，方便编程
+            #endregion
+
+
+            //c.隐式类型转换
+            Byte b1 = 250;
+            b1 += 10; // 快捷运算符不会做类型提升，所以不保存
+            // b1 = b1 + 10; // 二元运算符会做类型提升，这里可能出现内存溢出，所以报错
+
         }
     }
 }
