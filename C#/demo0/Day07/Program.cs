516b64c8ae28a22daf561ac178898d5c13f4718a
zhfei <1174977961@qq.com>
2023-05-23 22:49:49 +0800

    feat: C#自动拆箱，装箱

---
+++ C#/demo0/Day07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day07
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //装箱，拆箱
+
+            //装箱
+            int a = 10;
+            object obj = a;
+            Console.WriteLine(a);
+
+            //拆箱
+            int b = (int)obj;
+            Console.WriteLine(b);
+
+            //出现装箱，拆箱现象一般出现在：一个函数的参数是obect引用类型，传参时传递了int基本类型，导致装箱
+            //解决这类问题的方法：使用函数重载，泛型来避免
+        }
+    }
+}
