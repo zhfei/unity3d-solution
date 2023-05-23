8a3e6fec8828e0121bb2168848941ec8d98324c4
zhfei <1174977961@qq.com>
2023-05-23 23:08:14 +0800

    add: 添加枚举

--- C#/demo0/Day07/Program.cs
+++ C#/demo0/Day07/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Text;
 
 namespace Day07
 {
+    [Flags] //表示枚举可以执行与操作
+    enum PersonStyle {
+        tail = 1,
+        rich = 2,
+        handsome = 4,
+        white = 8
+    }
+
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main1(string[] args)
         {
+            #region
             //装箱，拆箱
 
             //装箱
@@ -19,6 +29,45 @@ namespace Day07
 
             //出现装箱，拆箱现象一般出现在：一个函数的参数是obect引用类型，传参时传递了int基本类型，导致装箱
             //解决这类问题的方法：使用函数重载，泛型来避免
+            #endregion
+        }
+
+        public static void Main(string[] args)
+        {
+            //可变字符串，避免频繁执行拆箱，装箱，造成内存垃圾
+            StringBuilder sb = new StringBuilder(10);
+            for (int i = 0; i < 10; i++)
+            {
+                sb.Append(i);
+            }
+            string res = sb.ToString();
+            Console.WriteLine(res);
+
+            Func1(PersonStyle.handsome);
+        }
+
+        private static void Func1(PersonStyle ps)
+        {
+            switch (ps)
+            {
+                case PersonStyle.handsome:
+                    Console.WriteLine("大智慧");
+                    return;
+                default:
+                    break;
+            }
+
+            //按位与，或判断
+            if ((ps & PersonStyle.handsome) != 0)
+            {
+                Console.WriteLine("大智慧");
+            }
+            if ((ps & PersonStyle.white) == PersonStyle.white)
+            {
+                Console.WriteLine("肤白貌美");
+            }
+
+
         }
     }
 }
