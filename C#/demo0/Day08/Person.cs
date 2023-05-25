44b03ebe377fdfa93d98c4736371d80afbbd6316
zhfei <1174977961@qq.com>
2023-05-25 22:04:49 +0800

    add: 添加类

---
+++ C#/demo0/Day08/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Day08
+{
+    public class Person
+    {
+        //私有字段，成员变量
+        private string name;
+        private int age;
+
+        //公有属性，用于get,set私有成员变量
+        public string Name {
+            get {
+                return this.name;
+            }
+
+            set {
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        // 构造方法
+        public Person()
+        {
+
+        }
+    }
+}
---
+++ C#/demo0/Day08/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Day08
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person p = new Person();
+            p.Name = "hanmeimei";
+            p.Age = 20;
+            Console.WriteLine(p);
+        }
+    }
+}
