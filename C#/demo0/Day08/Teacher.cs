7910d8efbecf44697c70b5a41c2cac698f43ca59
zhfei <1174977961@qq.com>
2023-05-27 00:43:03 +0800

    add: 添加struct结构体

---
+++ C#/demo0/Day08/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Day08
+{
+    //结构体和类的使用方法几乎一样，区别：类在堆中保存，结构体在栈中保存。
+    public struct Teacher
+    {
+        private string name;
+        //结构体中只有static, const修饰的字段可以被初始化。
+        private static int age = 0; 
+        public int Age { get; set; }
+    }
+}
