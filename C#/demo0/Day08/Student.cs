83e691f3195f873fcca313ca4abbe089f5a42ae2
zhfei <1174977961@qq.com>
2023-05-27 15:50:46 +0800

    add: 添加static静态构造方法

--- C#/demo0/Day08/Student.cs
+++ C#/demo0/Day08/Student.cs
@@ -3,10 +3,18 @@ namespace Day08
 {
     public class Student: Person
     {
-        public static int score = 10;
+        public static int score;
 
+        //静态构造方法，只在类对象被创建时调用一次
+        static Student()
+        {
+            //初始化类的静态变量
+            score = 10;
+        }
+        //实例构造方法
         public Student()
         {
+            //初始化类的实例成员变量
         }
     }
 }
