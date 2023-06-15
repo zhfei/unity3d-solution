fab33ca72228f5de421cf00b4a2ebbf1a2ecdd9d
zhfei <1174977961@qq.com>
2023-06-15 23:42:45 +0800

    add:添加英雄无敌

---
+++ HeroInvincible/Assets/Scripts/Enemy/EnemyMotor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 敌人马达：用于前进，旋转，寻路
+///
+/// </summary>
+
+public class EnemyMotor : MonoBehaviour
+{
+    /// <summary>
+    /// 前进
+    /// </summary>
+    public void MoveForward()
+    {
+        return;
+    }
+
+    /// <summary>
+    /// 面向目标点旋转
+    /// </summary>
+    /// <param name="targetPoint"></param>
+    public void LookRotation(Vector3 targetPoint)
+    {
+        return;
+    }
+
+    /// <summary>
+    /// 寻路，判断当前位置是否为终点
+    /// </summary>
+    /// <returns></returns>
+    public bool Pathfinding()
+    {
+        return false;
+    }
+}
