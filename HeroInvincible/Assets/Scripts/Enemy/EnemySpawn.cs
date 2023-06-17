14a8de803bba45ee6139a0cf43231b2c5201b321
zhfei <1174977961@qq.com>
2023-06-17 18:40:09 +0800

    add: 添加路线，敌人生成器

---
+++ HeroInvincible/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+///敌人生成器：提供生成敌人的功能
+/// 
+/// </summary>
+
+public class EnemySpawn : MonoBehaviour
+{
+
+    public int maxCount = 5;
+
+    private int startCount = 2;
+
+    private int currentCount = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
