8184628390fb07d4a1771591f846669a443b2f68
zhfei <1174977961@qq.com>
2023-06-25 21:58:05 +0800

    modify: 完善鼠标，键盘操作

--- BaseScript/Assets/Scripts/CameraRationDemo.cs
+++ BaseScript/Assets/Scripts/CameraRationDemo.cs
@@ -9,6 +9,8 @@ using UnityEngine;
 
 public class CameraRationDemo : MonoBehaviour
 {
+    public float speed = 40;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +20,30 @@ public class CameraRationDemo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void FixedUpdate()
+    {
         // 摄像头上下，左右旋转
         float resY = Input.GetAxisRaw("Mouse Y");
         float resX = Input.GetAxisRaw("Mouse X");
-        this.transform.Rotate(resY * 40, resX * 40, 0);
+
+        //上下旋转要选择自身坐标系
+        this.transform.Rotate(-resY * speed, 0, 0);
+        //左右旋转要选择世界坐标系
+        this.transform.Rotate(0, resX * speed, 0, Space.World);
         //print("resY: " + resY + "  resX: " + resX);
 
         // 摄像头前后，左右移动
-        float moveH = Input.GetAxisRaw("Horizontal");
-        float moveV = Input.GetAxisRaw("Vertical");
-        this.transform.Translate(moveH * Time.deltaTime, 0, moveV * Time.deltaTime);
-        print("moveH: " + moveH + "  moveV: " + moveV);
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        this.transform.Translate(moveH, 0, moveV);
+        //print("moveH: " + moveH + "  moveV: " + moveV);
+
+        //鼠标滚轮
+        float scrollV = Input.GetAxis("Mouse ScrollWheel");
+        print("scrollV: " + scrollV);
+
     }
 }
