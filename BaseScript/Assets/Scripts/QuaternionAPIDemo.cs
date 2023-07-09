4f7fdb82e4dd9875067e696e05d739b8b51ff7f5
zhfei <1174977961@qq.com>
2023-07-09 17:55:28 +0800

    feat: 完善四元数API使用

--- BaseScript/Assets/Scripts/QuaternionAPIDemo.cs
+++ BaseScript/Assets/Scripts/QuaternionAPIDemo.cs
@@ -27,30 +27,46 @@ public class QuaternionAPIDemo : MonoBehaviour
 
     // Update is called once per frame
     public Transform target;
+    
+    private void OnGUI()
+    {
+        Quaternion dir = Quaternion.LookRotation(target.position - transform.position);
+        if (GUILayout.RepeatButton("LookRotation+++++++++++++++++++"))
+        {
+            //4.注视旋转
+            //方法1
+            //Quaternion dir2 = Quaternion.LookRotation(target.position - transform.position);
+            //transform.rotation = dir2;
+            //方法2
+            transform.LookAt(target.position);
+        }
+        if (GUILayout.RepeatButton("Lerp"))
+        {
+            //5.Lerp差值旋转，由快到慢
+            transform.rotation = Quaternion.Lerp(transform.rotation, dir, 0.1f);
+        }
+        if (GUILayout.RepeatButton("RotateTowards"))
+        {
+            //6.RotateTowards: 匀速旋转
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, dir, 0.1f);
+        }
+        if (GUILayout.RepeatButton("Angle角度判断"))
+        {
+            Quaternion dir2 = Quaternion.Euler(0, 180, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, dir2, 0.005f);
+            //7.2个四元数角度差计算
+            if (Quaternion.Angle(transform.rotation, dir2) < 30)
+            {
+                transform.rotation = dir2;
+            }
+        }
+        
+    }
+
     void Update()
     {
-        //4.注视旋转
-        Quaternion dir = Quaternion.LookRotation(target.position - transform.position);
-        transform.rotation = dir;
-        transform.LookAt(target.position);
-
-        //5.Lerp差值旋转，由快到慢
-        transform.rotation = Quaternion.Lerp(transform.rotation,dir,0.1f);
-
-        //6.RotateTowards: 匀速旋转
-        transform.rotation = Quaternion.RotateTowards(transform.rotation,dir,0.1f);
-
-
-        Quaternion dir2 = Quaternion.Euler(0,180,0);
-        transform.rotation = Quaternion.Lerp(transform.rotation,dir2,0.0005f);
-        //7.2个四元数角度差计算
-        if (Quaternion.Angle(transform.rotation,dir2) < 30)
-        {
-            transform.rotation = dir2;
-        }
-
         //8.从？到？的旋转
-        transform.rotation = Quaternion.FromToRotation(Vector3.right,target.position - transform.position);
+        //transform.rotation = Quaternion.FromToRotation(Vector3.right, target.position - transform.position);
 
     }
 }
