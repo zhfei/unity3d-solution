36b5195baf2693a52a68d8bad9836b49ae401465
zhfei <1174977961@qq.com>
2023-07-20 00:08:53 +0800

    add: 完善碰撞器

--- BaseScript/Assets/Scripts/ColisionDemo.cs
+++ BaseScript/Assets/Scripts/ColisionDemo.cs
@@ -22,17 +22,19 @@ public class ColisionDemo : MonoBehaviour
         
     }
 
-    // 碰撞开始时，触发回调
+    // 碰撞开始时，接触的第一帧，触发回调
     private void OnCollisionEnter(Collision collision)
     {
-        
+        //通过collision.collider拿到了另一碰撞对象的碰撞器，那么就可以通过这个碰撞器获取这个对象上所有的其他组件。
+        //collision.collider.GetComponent<MeshRenderer>();
+        Debug.Log(collision.collider.name);
     }
 
     private void OnCollisionStay(Collision collision)
     {
         
     }
-    // 碰撞结束时，触发回调
+    // 碰撞结束时，接触的最后一帧，触发回调
     private void OnCollisionExit(Collision collision)
     {
         
