ce1136cceae0e0b3c1cc7e16fce1c8d23c8de538
zhfei <1174977961@qq.com>
2023-07-19 23:55:42 +0800

    add: 添加碰撞检测

---
+++ BaseScript/Assets/Scripts/ColisionDemo.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 碰撞检测
+///
+/// </summary>
+
+public class ColisionDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 碰撞开始时，触发回调
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+    // 碰撞结束时，触发回调
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+}
