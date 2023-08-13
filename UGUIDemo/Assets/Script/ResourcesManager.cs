45522c0d1c02125599db94fdad05f41020d35347
zhfei <1174977961@qq.com>
2023-08-13 22:39:31 +0800

    add：添加精灵资源管理器ResourcesManager

---
+++ UGUIDemo/Assets/Script/ResourcesManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 精灵资源管理器
+/// </summary>
+
+public class ResourcesManager
+{
+    private static Sprite[] spriteList;
+    //静态构造函数，调用时机是在类加载的时候被调用
+    static ResourcesManager()
+    {
+        //获取精灵，把精灵设置到Image组件中
+        //加载Resources资源，资源必须放在Resources目录下面
+        //单个精灵加载
+        //Sprite sprite = Resources.Load<Sprite>("2048Atlas");
+        //加载精灵图集
+        spriteList = Resources.LoadAll<Sprite>("2048Atlas");
+    }
+
+    /// <summary>
+    /// 读取数字精灵
+    /// </summary>
+    /// <param name="number">精灵名称对应的数字</param>
+    /// <returns>精灵</returns>
+    public static Sprite? LoadSprite(int number)
+    {
+        
+        foreach (var item in spriteList)
+        {
+            if (item.name == number.ToString())
+            {
+                return item;
+            }
+        }
+        return null;
+         
+    }
+}
