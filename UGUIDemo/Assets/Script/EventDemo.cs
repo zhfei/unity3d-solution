a12b3cd197b41bf3338f6e351141d4ee6c4fdc60
zhfei <1174977961@qq.com>
2023-08-30 22:59:47 +0800

    add：绑定Btn的点击事件

---
+++ UGUIDemo/Assets/Script/EventDemo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+///
+/// </summary>
+
+public class EventDemo : MonoBehaviour
+{
+    public void Func1()
+    {
+        print("Func1");
+    }
+}
