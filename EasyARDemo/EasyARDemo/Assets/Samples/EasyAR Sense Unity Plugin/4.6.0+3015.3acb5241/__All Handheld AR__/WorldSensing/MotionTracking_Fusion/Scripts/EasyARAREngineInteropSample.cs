47f2d874378b894ff31544a8ffef00d90c728dee
zhfei <1174977961@qq.com>
2024-01-31 00:33:03 +0800

    feat: 添加ARDemo

---
+++ EasyARDemo/EasyARDemo/Assets/Samples/EasyAR Sense Unity Plugin/4.6.0+3015.3acb5241/__All Handheld AR__/WorldSensing/MotionTracking_Fusion/Scripts/EasyARAREngineInteropSample.cs
@@ -0,0 +1,14 @@
+﻿//================================================================================================================================
+//
+//  Copyright (c) 2015-2023 VisionStar Information Technology (Shanghai) Co., Ltd. All Rights Reserved.
+//  EasyAR is the registered trademark or trademark of VisionStar Information Technology (Shanghai) Co., Ltd in China
+//  and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
+//
+//================================================================================================================================
+
+namespace Sample
+{
+    public class EasyARAREngineInteropSample : EasyARARCoreARKitWrapperSample
+    {
+    }
+}
