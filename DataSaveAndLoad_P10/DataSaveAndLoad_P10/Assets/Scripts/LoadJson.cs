775d14fea4980b835641e2676c943d6242bc327a
zhoufei <1174977961@qq.com>
2024-04-23 13:49:22 +0800

    feat: 添加loadJson方法

--- DataSaveAndLoad_P10/DataSaveAndLoad_P10/Assets/Scripts/LoadJson.cs
+++ DataSaveAndLoad_P10/DataSaveAndLoad_P10/Assets/Scripts/LoadJson.cs
@@ -23,24 +23,62 @@ public class LoadJson : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Dictionary<string, ItemJson> dict;
-
+        //Items文件必须要有json后缀，比如：Items.json 才能识别
         TextAsset jsonText = Resources.Load<TextAsset>("JSON/Items");
 
-        Debug.Log("jsonText:"+ jsonText.ToString());
+        Debug.Log("jsonText:"+ LoadJSON2(jsonText.text));
 
-        dict = JsonMapper.ToObject<Dictionary<string, ItemJson>>(jsonText.text);
-
-        foreach(var pair in dict)
-        {
-            Debug.LogFormat("{0}:{1}, {2}, {3}",pair.Key,pair.Value.id,
-               pair.Value.name, pair.Value.attack);
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //使用litJSON反序列化
+    //JsonMapper：批量反序列化
+    void LoadJSON1()
+    {
+        Dictionary<string, ItemJson> dict;
+
+        TextAsset jsonText = Resources.Load<TextAsset>("JSON/Items");
+
+        Debug.Log("jsonText:" + jsonText);
+
+        dict = JsonMapper.ToObject<Dictionary<string, ItemJson>>(jsonText.text);
+
+        foreach (var pair in dict)
+        {
+            Debug.LogFormat("{0}:{1}, {2}, {3}", pair.Key, pair.Value.id,
+               pair.Value.name, pair.Value.attack);
+        }
+    }
+
+    //逐个处理
+    //LitJSON对类型匹配要求严格，使用单个处理可以进行类型转换
+    Dictionary<string, ItemJson> LoadJSON2(string text)
+    {
+        Dictionary<string, ItemJson> dict = new Dictionary<string, ItemJson>();
+
+        JsonData jsonData = JsonMapper.ToObject(text);
+
+        foreach(KeyValuePair<string, JsonData> pair in jsonData)
+        {
+            int id = int.Parse(pair.Key);
+
+            ItemJson item = new ItemJson();
+            item.id = id;
+            item.name = (string)pair.Value["name"];
+            item.type = (int)pair.Value["type"];
+            item.attack = float.Parse(pair.Value["attack"].ToString());
+            item.defence = float.Parse(pair.Value["defence"].ToString());
+            item.data = float.Parse(pair.Value["data"].ToString());
+
+            dict.Add(id.ToString(),item);
+        }
+
+        return dict;
     }
 }
