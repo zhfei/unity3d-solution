bada9ec193ebb68a0614cb195ba1f1688d88847d
zhoufei <1174977961@qq.com>
2024-04-24 18:16:46 +0800

    feat：添加游戏AI

---
+++ GameAI_P11/GameAI_P11/Assets/MoveTo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
