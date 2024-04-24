5501e512b9c0bf7f3154dcf2c090abf03335ef8f
zhoufei <1174977961@qq.com>
2024-04-24 12:02:52 +0800

    feat: 声明属性，使Unity编辑器可修改。

--- DataSaveAndLoad_P10/DataSaveAndLoad_P10/Assets/Scripts/BinarySerializableTest.cs
+++ DataSaveAndLoad_P10/DataSaveAndLoad_P10/Assets/Scripts/BinarySerializableTest.cs
@@ -26,6 +26,9 @@ public class BinarySerializableTest : MonoBehaviour
 
     public PlayerData2 testPD;
 
+    [SerializeField]
+    private string name;
+
     // Start is called before the first frame update
     void Start()
     {
